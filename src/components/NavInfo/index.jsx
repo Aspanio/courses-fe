84b196d48dea39bddcfbe18a4df1521581ebd5e4
aspanio <aspanio@outlook.com>
2019-03-11 10:01:59 +0200

    NavBar

--- src/components/NavInfo/index.jsx
+++ src/components/NavInfo/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from './styles.module.scss'
 
 // import PropTypes from 'prop-types';
 // import { connect } from 'react-redux'
@@ -14,7 +15,15 @@ class NavInfo extends Component {
 
   render() {
     return (
-      <div>123</div>
+      <div className={styles.navInfoContainer}>
+        <div className={styles.navInfoContent}>
+          <h4><a href="#1">Главная</a></h4>
+          <div className={styles.line} />
+          <h4><a href="#1">Курсы</a> </h4>
+          <div className={styles.line} />
+          <h4><a href="#1">Состав курса</a> </h4>
+        </div>
+      </div>
     );
   }
 }
